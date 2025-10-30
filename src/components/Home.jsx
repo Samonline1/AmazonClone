3bc6c054da9652839978350f88ddcdb7c0fa01cc
Samonline1 <mrsampvt@gmail.com>
2025-10-30 16:46:30 +0530

    Created Navbar and slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 
+
 const Home = () => {
+
+ 
+
+
   return (
-    <div className='h-[100vh] w-screen overflow-x-hidden box-border bg-red-600'
-    >Home</div>
+    <div className='h-[100vh] w-full overflow-x-hidden box-border bg-red-600'
+    >
+
+       <div className="flex w-full  [&::-webkit-scrollbar]:hidden scrollbar-none scroll-Ani">
+        
+       
+            <img className="w-full"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img24/Media/BAU/PC_Hero_1x-toys._CB582765723_.jpg"
+            alt=""
+          />
+
+          <img className="w-full"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/MA2025/GW/BAU/Unrec/PC/934044815._CB551384116_.jpg"
+            alt=""
+          />
+          <img className="w-full"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/2025/LawnGarden/OCTOBER2025/GATEWAY/30thOCT/Door-and-bed-nets-GW-Hero-Pc._CB779327545_.jpg"
+            alt=""
+          />
+          <img className="w-full"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/CEPC/Jupiter/61/updated/PC_PB_Leadup_ASIN_Date._CB801963094_.jpg"
+            alt=""
+          />
+          <img className="w-full"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/uber_new_high._CB537689643_.jpg"
+            alt=""
+          />
+          <img className="w-full"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/Beauty/GW/yesbank/Shampoos__conditioners_pc._CB796616147_.png"
+            alt=""
+          />
+       
+       
+       
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,130 @@
-import React from 'react'
+import React, { useState } from "react";
+import { SlLocationPin } from "react-icons/sl";
+import { BsSearch } from "react-icons/bs";
+import ReactFlagsSelect from "react-flags-select";
+
+import { LuShoppingCart } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div className='w-full h-30 bg-yellow-500'>
-        Navbar
-    </div>
-  )
-}
+  const [input, setInput] = useState("");
+  const [selected, setSelected] = useState("IN");
 
-export default Navbar
+  const categories = [
+    { name: "All", link: "/all" },
+    { name: "Fresh", link: "/fresh", sub: ["Fruit", "Vegetables"] },
+    { name: "Electronics", link: "/electronics" },
+  ];
+
+  return (
+    <div className=" w-full h-24">
+      {/* main nav */}
+      <nav className="flex h-15 w-full p-4 mt-3 justify-between items-center space-x-5">
+        {/* <div className='flex w-full w-62 h-10' >
+         <img className='w-full h-full object-center flex-shrink-0' src="https://freelogopng.com/images/all_img/1688364164amazon-logo-transparent.png" alt="" srcSet="" /> 
+        </div> */}
+        {/* 
+        <div className="flex items-center">
+ <img
+  src="https://freelogopng.com/images/all_img/1688364164amazon-logo-transparent.png"
+  alt="Amazon logo"
+  className="w-20 sm:w-24 md:w-28 lg:w-32 h-auto object-contain"
+/>
+</div> */}
+
+        <div className="flex items-center w-32 h-12">
+          <img
+            src="https://freelogopng.com/images/all_img/1688364164amazon-logo-transparent.png"
+            alt="Amazon logo"
+            className="w-32 h-12 object-contain flex-shrink-0"
+          />
+        </div>
+
+        <div className="w-55">
+          <p className="text-sm">Delivering to Mumbai</p>
+          <div className="flex w-full">
+            <SlLocationPin />
+            <p className="font-bold">Update location</p>
+          </div>
+        </div>
+
+        <div className="flex w-[50%]  bg-white rounded-md outline-none border-none text-black">
+          <input
+            className=" w-full p-2 appearance-none outline-none border-none bg-transparent focus:ring-0 focus:outline-none focus:border-none text-black"
+            type="search"
+            placeholder="Search Amazon.in"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <button className="bg-yellow-500 text-white">
+            <BsSearch />
+          </button>
+        </div>
+
+        <div className="flex w-100 space-x-3">
+          <div className="text-white ">
+            <ReactFlagsSelect
+              selected={selected}
+              onSelect={(code) => setSelected(code)}
+              countries={["IN", "US", "GB", "FR", "DE", "ES"]} // Optional: limit to certain countries
+              customLabels={{
+                IN: "EN",
+                US: "EN",
+                GB: "EN",
+                FR: "FR",
+                DE: "DE",
+                ES: "ES",
+              }}
+              showSelectedLabel={true}
+              showSecondarySelectedLabel={false}
+              fullWidth={false}
+              selectButtonClassName="!border-0 !bg-transparent !shadow-none hover:!bg-transparent focus:!outline-none !text-white"
+            />
+
+            {/* <ReactFlagsSelect
+             className='w-20 ouline-none border-none'
+             selectButtonClassName="menu-flags-button"
+             fullWidth={false}
+ placeholder="Select Language"           
+            showOptionLabel={false}
+            showSecondarySelectedLabel={false}
+            showSelectedLabel={false}
+            customLabels={"US"}
+    selected={selected}
+    onSelect={(code) => setSelected(code)}
+  /> */}
+          </div>
+          <div>
+            <p className="text-sm">Hello, X</p>
+            <p className="font-bold">Account & Lists</p>
+          </div>
+          <div>
+            <p className="text-sm">Returns</p>
+            <p className="font-bold">& Orders</p>
+          </div>
+          <div className="text-[40px]">
+            <LuShoppingCart />
+          </div>
+        </div>
+      </nav>
+
+      {/* Category */}
+
+      <div>
+        <nav className="flex items-center p-4 bg-gray-900">
+          {/* <Link to={`/ok/${oka}`} className="link"
+      >
+       All
+      </Link> */}
+
+          {/* <a href={`/ok/${oka}`} className="text-white">
+  Go to
+</a> */}
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
